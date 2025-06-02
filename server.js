3a0f90e1185fbe88b97c49d3143649dd78dd4ca9
Marius Heașcă <marius_heasca@yahoo.com>
2025-06-02 22:36:11 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-// Serve static files from the 'public' folder
-app.use(express.static('public'));
-
-// Optional: fallback route
-app.get('*', (req, res) => {
-  res.status(404).send('404 - Not Found');
-});
+// Serve everything in "public"
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
